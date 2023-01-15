6ecd6b6efd3f6ee205b0375aae15a0d0dbe26d09
bicco2 <jhb_0921@naver.com>
2023-01-15 22:35:28 +0900

    feat : create customAxios

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,24 @@
 import { createClickEventAtLink } from "../utils/createClickEventAtLink";
 import { $ } from "../utils/querySelector";
+import { customAxios } from "../utils/customAxios";
 
 function Main($container) {
   this.$container = $container;
+  this.postData = "";
 
-  this.setState = () => {
+  customAxios
+    .get("/posts")
+    .then((res) => {
+      this.setState(res.data.data.posts);
+    })
+    .catch((error) => console.log(error));
+
+  this.setState = (state) => {
+    this.postData = state; // postData 값이 재할당 되면 재렌더링
     this.render();
   };
 
   this.render = () => {
-    const test = [1, 2, 3, 4];
     this.$container.innerHTML = `
       <section id="page-main">
         <div id="common-header">
@@ -26,28 +35,31 @@ function Main($container) {
 
       </section>
     `;
-    test.forEach((each) => {
-      $("#post-list").insertAdjacentHTML(
-        "beforeend",
-        `<li id="post-item">
+
+    if (this.postData !== "") {
+      this.postData.forEach((each) => {
+        $("#post-list").insertAdjacentHTML(
+          "beforeend",
+          `<li id="post-item">
         <a href="/post/12" id="link-to-post-main">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPgX7NT9rVjPDUUWMRSQ_6ZqdCRaKi92i_P_bf1YASGw&s">
           <div>
-            <strong>${each}</strong>
+            <strong>${each.postId}</strong>
             <p>내용</p>
           </div>
         </a>
       </li>`
-      );
-    });
+        );
+      });
+      const ATagToDetail = document.getElementById("link-to-post-main");
+      createClickEventAtLink(ATagToDetail);
+    }
 
     const ATagToHome = document.getElementById("link-to-main");
     const ATagToUpload = document.getElementById("create-post-link");
-    const ATagToDetail = document.getElementById("link-to-post-main");
 
     createClickEventAtLink(ATagToHome);
     createClickEventAtLink(ATagToUpload);
-    createClickEventAtLink(ATagToDetail);
   };
 
   this.render();
