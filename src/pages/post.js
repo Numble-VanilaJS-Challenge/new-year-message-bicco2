85c19ecbb5e6434830acd217bc58f9aaced432a5
bicco2 <jhb_0921@naver.com>
2023-01-16 22:44:56 +0900

    feat : comment delete & create

--- src/pages/post.js
+++ src/pages/post.js
@@ -5,6 +5,7 @@ function Post($container) {
   this.$container = $container;
   this.postData = "";
   this.commentDataList = "";
+  this.comment = "";
 
   this.setState = (postState, commentState) => {
     this.postData = postState;
@@ -29,7 +30,6 @@ function Post($container) {
       flex-direction: column;
       align-items: flex-start;
       justify-content: flex-start;
-
       padding-bottom: 64px;
       overflow: hidden;">
       <div id="common-header" style="justify-content: space-between;">
@@ -38,7 +38,6 @@ function Post($container) {
         <a href="/" id="link-to-upload-post2">HPNY 2023
         </a>
       </div>
-     
       <article id="post-article">
         <img src=${this.postData.image} id="post-img"/>
         <strong>${this.postData.title}</strong>
@@ -53,12 +52,11 @@ function Post($container) {
       </article>
       <section id="comment-section">
         <form>
-          <input></input>
-          <button type="submit">게시</button>
+          <input id="comment-input"></input>
+          <button type="submit" id="submit-btn">게시</button>
         </form>
         <ul id="comment-list">
         </ul>
-
       </section>
     </section>
     `;
@@ -66,15 +64,53 @@ function Post($container) {
     if (this.commentDataList !== "") {
       this.commentDataList.forEach((commentItem) => {
         $("#comment-list").insertAdjacentHTML(
-          "beforeend",
+          "beforebegin",
+
           `<li id="comment">
             <p>${commentItem.content}</p>
-            <p id="comment-id" style="visibility : hidden">${commentItem.commentId}</p>
-            <button id="comment-delete-btn">삭제</button>
+            <button id=${commentItem.commentId}>삭제</button>
           </li>`
         );
+        const deleteBtn = document.getElementById(commentItem.commentId);
+        deleteBtn.addEventListener("click", () => {
+          if (window.confirm("삭제하시겠습니까?")) {
+            customAxios
+              .delete(`/comment/${commentItem.commentId}`)
+              .then((res) => {
+                location.reload();
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          } else {
+          }
+        });
       });
     }
+
+    const commentInput = document.getElementById("comment-input");
+
+    commentInput.addEventListener("input", (e) => {
+      const commentValue = document.getElementById("comment-input").value;
+      this.comment = commentValue;
+    });
+
+    const submitBtn = document.getElementById("submit-btn");
+    submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      customAxios
+        .post(`/comment/${this.postData.postId}`, {
+          content: this.comment,
+        })
+        .then((res) => {
+          location.reload();
+        })
+        .catch((err) => {
+          if (err.request.status === 400) {
+            alert("잘못된 접근입니다.");
+          }
+        });
+    });
   };
 
   this.render();
